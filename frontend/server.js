29c16180b66ad1efc00ac63b89713eff07137452
Szilárd Antal <sziszo@gmail.com>
2019-11-17 11:36:28 +0100

    fixed routing on page refresh

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const compression = require("compression");
+const bodyParser = require('body-parser')
 
-const app = express();
+const _app_folder = 'dist/frontend';
 const port = process.env.PORT || 3000;
 
+const app = express();
+app.use(compression());
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/dist/frontend'));
-
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
@@ -17,6 +20,11 @@ app.all('/api/*', (req, res) => {
   apiProxy.web(req, res, {target: 'http://localhost:1234'});
 });
 
-const server = http.createServer(app);
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/frontend', 'index.html'));
+})
 
-server.listen(port, () => console.log('Running...'));
+// const server = http.createServer(app);
+// app.use(express.static(__dirname + '/dist/frontend'));
+
+app.listen(port, () => console.log('Running...'));
