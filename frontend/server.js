88e625ae6327a2240a68b6809c7c645a7f056822
Szilárd Antal <sziszo@gmail.com>
2019-11-17 15:26:36 +0100

    cleanup

--- frontend/server.js
+++ frontend/server.js
@@ -2,29 +2,26 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const compression = require("compression");
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-const _app_folder = 'dist/frontend';
+const _app_folder = path.join(__dirname, '/dist/frontend');
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(compression());
-app.use(bodyParser.json())
-app.use(express.static(__dirname + '/dist/frontend'));
+app.use(bodyParser.json());
+app.use(express.static(_app_folder));
 
 const httpProxy = require('http-proxy');
 const apiProxy = httpProxy.createProxyServer();
 
 app.all('/api/*', (req, res) => {
-  console.log('redirecting to Server1');
+  console.log('redirecting to api server');
   apiProxy.web(req, res, {target: 'http://localhost:1234'});
 });
 
 app.use(function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/frontend', 'index.html'));
-})
-
-// const server = http.createServer(app);
-// app.use(express.static(__dirname + '/dist/frontend'));
+  res.sendFile(path.join(_app_folder, 'index.html'));
+});
 
 app.listen(port, () => console.log('Running...'));
